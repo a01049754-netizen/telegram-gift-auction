74e0804a13774c9ec9430fee0ea5032fd6464984
a01049754-netizen <a01049754@gmail.com>
2026-01-18 19:12:14 +0500

    Update auction.module.ts
    fix: register auction mongoose schema

--- backend/src/modules/auction/auction.module.ts
+++ backend/src/modules/auction/auction.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AuctionController } from './auction.controller';
 import { AuctionService } from './auction.service';
+import { Auction, AuctionSchema } from './auction.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Auction.name, schema: AuctionSchema },
+    ]),
+  ],
   controllers: [AuctionController],
   providers: [AuctionService],
+  exports: [AuctionService],
 })
 export class AuctionModule {}
