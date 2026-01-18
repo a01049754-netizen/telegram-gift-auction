08aad4ef327dd6186853904b5fae3052eb60e213
a01049754-netizen <a01049754@gmail.com>
2026-01-18 18:37:47 +0500

    Delete backend/src/bid directory

--- backend/src/bid/bid.schema.ts
+++
@@ -1,8 +0,0 @@
-import { Schema } from 'mongoose';
-
-export const BidSchema = new Schema({
-  auctionId: { type: String, index: true },
-  userId: String,
-  amount: Number,
-  createdAt: { type: Date, default: Date.now },
-});
