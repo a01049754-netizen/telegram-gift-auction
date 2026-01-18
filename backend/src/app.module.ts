7e1e67c518bfa1d5b162e421c8e6252febb2c5f2
a01049754-netizen <a01049754@gmail.com>
2026-01-18 19:09:04 +0500

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+
 import { AuctionModule } from './modules/auction/auction.module';
 import { BidModule } from './modules/bid/bid.module';
+import { BalanceModule } from './modules/balance/balance.module';
 
 @Module({
   imports: [
     AuctionModule,
     BidModule,
+    BalanceModule,
   ],
 })
 export class AppModule {}
