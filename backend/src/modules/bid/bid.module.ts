d0d625b61ce502f9df37e5b59c640fc0d08708fd
a01049754-netizen <a01049754@gmail.com>
2026-01-18 19:14:28 +0500

    Update bid.module.ts

--- backend/src/modules/bid/bid.module.ts
+++ backend/src/modules/bid/bid.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Bid, BidSchema } from './bid.schema';
+import { Auction, AuctionSchema } from '../auction/auction.schema';
 import { BidService } from './bid.service';
 import { BidController } from './bid.controller';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Bid.name, schema: BidSchema }]),
+    MongooseModule.forFeature([
+      { name: Bid.name, schema: BidSchema },
+      { name: Auction.name, schema: AuctionSchema },
+    ]),
   ],
   controllers: [BidController],
   providers: [BidService],
