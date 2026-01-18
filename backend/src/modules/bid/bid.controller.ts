41295ee93375e652f720cf8a11884f435afb727f
a01049754-netizen <a01049754@gmail.com>
2026-01-18 18:49:05 +0500

    Update bid.controller.ts

--- backend/src/modules/bid/bid.controller.ts
+++ backend/src/modules/bid/bid.controller.ts
@@ -1,16 +1,16 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { BidService } from './bid.service';
 
-@Controller('bids')
+@Controller('bid')
 export class BidController {
   constructor(private readonly bidService: BidService) {}
 
   @Post()
   placeBid(
-    @Body('userId') userId: string,
     @Body('auctionId') auctionId: string,
+    @Body('userId') userId: string,
     @Body('amount') amount: number,
   ) {
-    return this.bidService.placeBid(userId, auctionId, amount);
+    return this.bidService.placeBid(auctionId, userId, amount);
   }
 }
