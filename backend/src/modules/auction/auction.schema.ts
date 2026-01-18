394e0c02b8851b255cb7708c0fc388548b8123bd
a01049754-netizen <a01049754@gmail.com>
2026-01-19 04:29:24 +0500

    Update auction.schema.ts

--- backend/src/modules/auction/auction.schema.ts
+++ backend/src/modules/auction/auction.schema.ts
@@ -4,19 +4,20 @@ import { Document } from 'mongoose';
 @Schema({ timestamps: true })
 export class Auction extends Document {
   @Prop({ required: true })
-  title: string;
+  title!: string;
 
   @Prop({ required: true })
-  startPrice: number;
+  startPrice!: number;
 
   @Prop({ required: true })
-  endAt: Date;
+  endAt!: Date;
 
   @Prop({ default: true })
-  isActive: boolean;
+  isActive!: boolean;
 
+  // anti-sniping: можно продлить ТОЛЬКО ОДИН РАЗ (+5 сек)
   @Prop({ default: false })
-  extended: boolean; // anti-sniping flag
+  extended!: boolean;
 }
 
 export const AuctionSchema = SchemaFactory.createForClass(Auction);
